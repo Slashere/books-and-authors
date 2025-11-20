66bc8d5afe406d41a3b91c6056e6d6ba2709e236
Anton Fedorov <slashereimba@gmail.com>
2025-11-20 14:36:02 +0300

    Добавлена кнопка подписки на странице "Топ-10 авторов"

--- views/report/top-authors.php
+++ views/report/top-authors.php
@@ -20,7 +20,12 @@ if (empty($rows)) {
     echo "<p>Нет данных.</p>";
 } else {
     echo "<table class='table table-bordered'>";
-    echo "<tr><th>#</th><th>Автор</th><th>Количество книг</th></tr>";
+    echo "<tr>
+            <th>#</th>
+            <th>Автор</th>
+            <th>Количество книг</th>
+            <th>Подписка</th>
+          </tr>";
 
     $i = 1;
     foreach ($rows as $row) {
@@ -28,6 +33,13 @@ if (empty($rows)) {
         echo "<td>" . $i++ . "</td>";
         echo "<td>" . Html::encode($row['full_name']) . "</td>";
         echo "<td>" . (int)$row['book_count'] . "</td>";
+        echo "<td>"
+            . Html::a(
+                'Подписаться',
+                ['subscription/create', 'author_id' => $row['id']],
+                ['class' => 'btn btn-sm btn-success']
+            )
+            . "</td>";
         echo "</tr>";
     }
     echo "</table>";
